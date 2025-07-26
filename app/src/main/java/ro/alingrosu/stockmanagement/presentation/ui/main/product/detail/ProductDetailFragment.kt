ae3cb09b25206f4f2bef283ccd2dab033118cad3
grosualin <grosualin@gmail.com>
2025-07-26 12:53:11 +0300

    implemented ProductDetails fragment with view/edit functionality

--- app/src/main/java/ro/alingrosu/stockmanagement/presentation/ui/main/product/detail/ProductDetailFragment.kt
+++ app/src/main/java/ro/alingrosu/stockmanagement/presentation/ui/main/product/detail/ProductDetailFragment.kt
@@ -1,12 +1,17 @@
 package ro.alingrosu.stockmanagement.presentation.ui.main.product.detail
 
-import androidx.fragment.app.viewModels
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import ro.alingrosu.stockmanagement.R
 import ro.alingrosu.stockmanagement.databinding.FragmentProductDetailBinding
+import ro.alingrosu.stockmanagement.presentation.state.UiState
 import ro.alingrosu.stockmanagement.presentation.ui.base.BaseFragment
 import ro.alingrosu.stockmanagement.presentation.util.Factory
 import ro.alingrosu.stockmanagement.presentation.util.getAppComponent
@@ -14,6 +19,7 @@ import ro.alingrosu.stockmanagement.presentation.util.getAppComponent
 class ProductDetailFragment : BaseFragment(R.layout.fragment_product_detail) {
 
     private lateinit var binding: FragmentProductDetailBinding
+    private val args: ProductDetailFragmentArgs by navArgs()
 
     private val viewModel: ProductDetailViewModel by viewModels {
         Factory {
@@ -33,8 +39,48 @@ class ProductDetailFragment : BaseFragment(R.layout.fragment_product_detail) {
     }
 
     override fun initView() {
+        binding.apply {
+            etName.setText(args.product.name)
+            etDescription.setText(args.product.description)
+            etPrice.setText(args.product.price.toString())
+            etCategory.setText(args.product.category)
+            etBarcode.setText(args.product.barcode)
+            etSupplier.setText(args.product.supplier.name)
+            etCurrentStock.setText(args.product.currentStock.toString())
+            etMinStock.setText(args.product.minStock.toString())
+            buttonSave.setOnClickListener {
+                viewModel.saveProduct(
+                    args.product.copy(
+                        name = etName.text.toString(),
+                        description = etDescription.text.toString(),
+                        price = etPrice.text.toString().toDouble(),
+                        category = etCategory.text.toString(),
+                        barcode = etBarcode.text.toString()
+                    )
+                )
+            }
+        }
     }
 
     override fun listenFoUiState() {
+        viewModel.uiState.observe(viewLifecycleOwner) { uiState ->
+            when (uiState) {
+                is UiState.Loading -> {
+                    binding.loading.isVisible = true
+                    binding.buttonSave.isEnabled = false
+                }
+
+                is UiState.Success -> {
+                    binding.loading.isVisible = false
+                    binding.buttonSave.isEnabled = true
+                    Toast.makeText(context, getString(R.string.product_save_success), Toast.LENGTH_LONG).show()
+                    findNavController().popBackStack()
+                }
+
+                is UiState.Error -> {
+                    Toast.makeText(context, uiState.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/ro/alingrosu/stockmanagement/presentation/ui/main/product/detail/ProductDetailViewModel.kt
+++ app/src/main/java/ro/alingrosu/stockmanagement/presentation/ui/main/product/detail/ProductDetailViewModel.kt
@@ -1,11 +1,40 @@
 package ro.alingrosu.stockmanagement.presentation.ui.main.product.detail
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 import ro.alingrosu.stockmanagement.domain.usecase.ProductUseCase
+import ro.alingrosu.stockmanagement.presentation.mapper.toDomainModel
+import ro.alingrosu.stockmanagement.presentation.model.ProductUi
+import ro.alingrosu.stockmanagement.presentation.state.UiState
 import ro.alingrosu.stockmanagement.presentation.ui.base.BaseViewModel
 import javax.inject.Inject
 
 class ProductDetailViewModel @Inject constructor(
     private val productUseCase: ProductUseCase
 ) : BaseViewModel() {
+
+    private val _uiState = MutableLiveData<UiState<Boolean>>()
+    val uiState: LiveData<UiState<Boolean>> = _uiState
+
+    fun saveProduct(productUi: ProductUi) {
+        compositeDisposable.add(
+            productUseCase.updateProduct(productUi.toDomainModel())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe {
+                    _uiState.value = UiState.Loading
+                }
+                .doOnError { error ->
+                    _uiState.value = UiState.Error(error.message ?: "Unknown error")
+                }
+                .subscribe({
+                    _uiState.value = UiState.Success(true)
+                }, { error ->
+                    _uiState.value = UiState.Error(error.message ?: "Unexpected error occurred")
+                })
+        )
+    }
 
 }
